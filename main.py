bd97322b8d77fc7b2445f009f5a3cd92271886f5
Daniel Scheller <d.scheller@gmx.net>
2018-04-15 13:55:48 +0200

    license things, debug print

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 '''
-    Kodi LCDproc Python addon
-    Copyright (C) 2014 Team Kodi
-    Copyright (C) 2014 Daniel 'herrnst' Scheller
-    
+    Kodi LCDproc service
+
+    Copyright (C) 2018 Team Kodi
+    Copyright (C) 2018 Daniel 'herrnst' Scheller
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-    
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-    
+
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-    
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
@@ -73,6 +74,7 @@ def pymain():
 
   for thisthread in threadlist:
     thisthread.join()
+    log(LOGDEBUG, "mainthread: stopped %s" % thisthread)
 
 #########################################
 # python entry point
